704767d85bbb70a360760ebb8a9aeeb2f3ca8f37
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-02-05 20:09:50 -0500

    Create email.py

---
+++ email.py
@@ -0,0 +1,37 @@
+__author__ = 'Bernardo Augusto Godinho de Oliveira <@bernardogo>'
+
+
+import csv
+import mechanize
+import re
+import time
+import BeautifulSoup
+import smtplib
+
+def sendEmail(result, sfrom, pwd, sto, subject):
+    SMTP_SERVER = 'smtp.live.com'
+    SMTP_PORT = 587
+
+    sender = 'sfrom'
+    recipient = 'sto'
+    subject = 'subject'
+    body = result
+
+    body = "<b>" + body + "<b>"
+
+    headers = ["From: " + sender,
+               "Subject: " + subject,
+               "To: " + recipient,
+               "MIME-Version: 1.0",
+               "Content-Type: text/html"]
+    headers = "\r\n".join(headers)
+
+    session = smtplib.SMTP(SMTP_SERVER , SMTP_PORT)
+
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+    session.login(sender, pwd)
+
+    session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
+    session.quit()
